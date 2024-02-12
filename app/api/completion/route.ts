5f7afd5e2aafd6fd5a2a7970979d57d7721e0cf5
emersoftware <87254036+emersoftware@users.noreply.github.com>
2024-02-12 15:11:30 -0300

    Revert "Remove completion route"
    This reverts commit 65c1e3487fc81465a212ec1e903956188a48d3cd.

---
+++ app/api/completion/route.ts
@@ -0,0 +1,37 @@
+import OpenAI from 'openai';
+import {
+  OpenAIStream,
+  StreamingTextResponse,
+  experimental_StreamData,
+} from 'ai';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY || '',
+});
+
+export const runtime = 'edge';
+
+export async function POST(req: Request) {
+  const { prompt } = await req.json();
+
+  const response = await openai.completions.create({
+    model: 'gpt-3.5-turbo-instruct-0914',
+    temperature: 1,
+    max_tokens: 24,
+    stream: true,
+    prompt: "Continue the following sentence like an User input: " + prompt,
+  });
+
+  const data = new experimental_StreamData();
+
+  data.append({ test: 'value' });
+
+  const stream = OpenAIStream(response, {
+    onFinal(completion) {
+      data.close();
+    },
+    experimental_streamData: true,
+  });
+
+  return new StreamingTextResponse(stream, {}, data);
+}
